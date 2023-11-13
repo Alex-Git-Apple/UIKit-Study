2e682f01e7b801463e8ef74140f4ff7e1f885625
Pin <just4sport30@gmail.com>
2023-11-13 15:12:39 -0800

    Add the MainVC and MainCoordinator.

---
+++ Patterns/CoordinatorPattern/CoordinatorPattern/ViewControllers/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorPattern
+//
+//  Created by Pin Lu on 11/13/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        title = "The Main View"
+    }
+
+}
+
