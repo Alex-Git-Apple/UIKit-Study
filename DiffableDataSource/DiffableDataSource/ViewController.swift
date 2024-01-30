fac25e0f2c6adbedfe920698ce9f1bb5c6e75211
Pin <just4sport30@gmail.com>
2024-01-30 08:44:05 -0800

    Set up project and rmove storyboard.

---
+++ DiffableDataSource/DiffableDataSource/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DiffableDataSource
+//
+//  Created by Pin Lu on 1/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .orange
+    }
+
+
+}
+
