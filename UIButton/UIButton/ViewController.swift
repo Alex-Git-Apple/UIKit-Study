92edb2fec76660c14c9423620eb0e425f2f21ed2
Alex <just4sport30@gmail.com>
2024-06-05 19:09:03 -0700

    Button with UIMenu.

--- UIButton/UIButton/ViewController.swift
+++ UIButton/UIButton/ViewController.swift
@@ -10,13 +10,20 @@ import UIKit
 class ViewController: UIViewController {
     
     var button1: UIButton!
+    
+    var button2: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupButton1()
+        setupButton2()
+        layout()
+    }
+    
+    func setupButton1() {
         setup()
         style()
-        layout()
     }
     
     func setup() {
@@ -52,11 +59,44 @@ class ViewController: UIViewController {
         }
     }
     
+    // Button with UIMenu
+    func setupButton2() {
+        button2 = UIButton(type: .system)
+        var config = UIButton.Configuration.bordered()
+        config.title = "Menu Button"
+        config.buttonSize = .large
+        
+        button2.configuration = config
+        
+        view.addSubview(button2)
+        
+        // Create the menu items
+        let shareAction = UIAction(title: "Share", handler: { _ in
+          // Share functionality here
+            self.button2.configuration?.title = "Share"
+        })
+        let deleteAction = UIAction(title: "Delete") { action in
+          // Delete functionality here
+            self.button2.configuration?.title = "Delete"
+        }
+
+        // Create the menu
+        let menu = UIMenu(title: "Options", options: .displayInline, children: [shareAction, deleteAction])
+
+        // Set the menu on the button and enable primary action
+        button2.menu = menu
+        button2.showsMenuAsPrimaryAction = true
+    }
+    
     func layout() {
         button1.translatesAutoresizingMaskIntoConstraints = false
+        button2.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             button1.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: 2),
-            button1.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            button1.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            button2.topAnchor.constraint(equalToSystemSpacingBelow: button1.bottomAnchor, multiplier: 5),
+            button2.centerXAnchor.constraint(equalTo: view.centerXAnchor),
         ])
     }
 
